f8448bc77601d72209c57d4c1d669c65c32b5c56
Jacques Gomes <113613459+JacquesGomes@users.noreply.github.com>
2023-03-09 08:12:54 -0300

    Initial commit

---
+++ basics.sh
@@ -0,0 +1,16 @@
+#"!x" repeats the last command starting with 'x'
+
+#"!!" repeat the last command, useful for when the user forgets sudo - sudo !!
+
+#script -> write code -> exit (reusable by using "cat typescript") -> by passing an argument you can name tha script and move it for a folder to be reused
+
+#"cat > "file name" -> start writing -> ctrl + d to finish - use as text editor
+
+#"mv file name {.old,.new} -> change a file extension"
+
+#append text to a file = command >> file. ex: echo build new server >> todo.txt
+
+#cat + filename -> show text
+
+#"tree" to view files as tree. -d to only see directories and -L + number to limit the list.
+
